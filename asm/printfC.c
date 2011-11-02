e34a839d5281754b642d03417547f6e3354c8c9e
Collin <collin.reeser@gmail.com>
2011-11-02 17:39:30 -0400

    Implemented a print double function for printf_cr. The only issue plaguing the print float and print double functions right now are printing very large integer components. Printing very lengthy fractional components works perfectly, mostly to do with the rounding of the float itself. Testing needs to be done for very, very small fractional components though, like 0.0000000000000000000012. In addition, printing very large integer components of the floating point values also needs to be fixed.

--- asm/printfC.c
+++ asm/printfC.c
@@ -9,6 +9,7 @@
 #define NUM 'd'
 #define FLT 'f'
 #define CHR 'c'
+#define DUB 'b'
 
 // Need defines for flags that go along with the different command directives,
 // and to implement a parsing system
@@ -18,6 +19,7 @@ extern void putchar_cr(char x);
 extern int fltBitsToInt(float num);
 void printI(int num);
 void printF(double num);
+void printDbl(double num);
 
 
 // Dammit. Well. I guess I can do this in C then, jerks.
@@ -44,6 +46,9 @@ void printf_cr(char* buf , ...)
 					case FLT:
 						printF(va_arg(args, double));
 						break;
+					case DUB:
+						printDbl(va_arg(args, double));
+						break;	
 					case CHR:
 						break;
 					default:
@@ -114,11 +119,58 @@ void printI(int num)
 	return;
 }
 
-
+void printL(long long num)
+{
+	char zeroTrip = '1';
+	long long divisor;
+	char mod;
+	// First print a negative sign if needed
+	if ( num < 0 )
+	{
+		putchar_cr('-');
+		num *= -1;
+	}
+	if (sizeof(long long) == 8)
+	{
+		// Max val of int is 2147483647
+		divisor = 1000000000000000000;
+	}
+	else if (sizeof(int) == 2)
+	{
+	}
+	else if (sizeof(int) == 8)
+	{
+	}
+	// Using decreasing divisors, divide the num by the divisor, where the
+	// quotient must be the digit to print. Ignore leading zeros. The remainder
+	// of each operation is the number without its most significant digit,
+	// meaning we can iteratively print each digit in the number from left to
+	// right
+	for ( ; divisor >= 1; divisor /= 10)
+	{
+		mod = num / divisor;
+		num %= divisor;
+		if ( mod == 0 )
+		{
+			if ( zeroTrip != '1' )
+			{
+				putchar_cr(mod + '0');
+			}
+		}
+		else
+		{
+			zeroTrip = '0';
+			putchar_cr(mod + '0');
+		}
+	}
+	return;
+}
 
 
 void printF(double num)
 {
+	char floatSec[10];
+	floatSec[9] = 0;
 	float fVal = (float)num;
 	//printf("%f << Original value\n",fVal);
 	unsigned int intRep = fltBitsToInt(fVal);
@@ -126,20 +178,80 @@ void printF(double num)
 	unsigned int sign = intRep >> 31;
 	//printf("Sign:%u\n",sign);
 	unsigned int mantissa = ( intRep << 1 ) >> 24;
-	//printf("Mantissa:Octal:%o\nMantissa:Dec:%u\n",mantissa);
-	unsigned int floating = ( ( intRep << 9 ) >> 9 ) | 0b100000000000000000000000;
+	//printf("Mantissa:Octal:%o\nMantissa:Dec:%u\n",mantissa,mantissa);
+	unsigned int floating = ( ( intRep << 9 ) >> 9 );// | 0b100000000000000000000000;
 	//printf("Float:%u\n",floating);
+	//printf("Float:%o\n",floating);
 	if ( sign != 0 )
 	{
 		putchar_cr('-');
 	}
-	unsigned int exponent = mantissa - 127;
-	//printf("Exponent:%d\n",exponent);
-	int integerComponent = (floating >> (23 - exponent));
-	unsigned int floatComponent = floating << ( 9 + exponent );
-	//printf("Integer component:%d\n",integerComponent);
+	// 0 condition
+	if ( floating == 0 && mantissa == 0 )
+	{
+		floatSec[0] = '0';
+		floatSec[1] = '.';
+		floatSec[2] = '0';
+		floatSec[3] = '0';
+		floatSec[4] = '0';
+		floatSec[5] = '0';
+		floatSec[6] = '0';
+		floatSec[7] = '0';
+		floatSec[8] = 0;
+		fputs_cr(floatSec);
+		return;
+	}
+	// Infinite, and NaN
+	if ( mantissa == 0b11111111 )
+	{
+		// Infinite
+		if ( floating == 0 )
+		{
+			floatSec[0] = 'i';
+			floatSec[1] = 'n';
+			floatSec[2] = 'f';
+			floatSec[3] = 'i';
+			floatSec[4] = 'n';
+			floatSec[5] = 'i';
+			floatSec[6] = 't';
+			floatSec[7] = 'e';
+			floatSec[8] = 0;
+			fputs_cr(floatSec);
+			return;
+		}
+		// NaN, for undefined operations or uninitialized values
+		else
+		{
+			floatSec[0] = 'N';
+			floatSec[1] = 'a';
+			floatSec[2] = 'N';
+			floatSec[3] = 0;
+			fputs_cr(floatSec);
+			return;
+		}
+	}
+	floating |= 0b100000000000000000000000;
+	//floating |= 0b000000000000000000000000;
+	int exponent = mantissa - 127;
+	printf("Floating: %u\n",floating);
+	printf("Exponent: %d\n",exponent);
+	int integerComponent;
+	unsigned int floatComponent;
+	if ( exponent < 24 )
+	{
+		integerComponent = (floating >> (23 - exponent));
+		floatComponent = ( floating << ( 9 + exponent ) );
+	}
+	else if ( exponent > 23  && exponent < 32)
+	{
+		integerComponent = (floating << (exponent - 23));
+		floatComponent = ( floating << ( 9 + exponent ) );
+	}
+	printf("Integer component: %d\n",integerComponent);
+	printf("Float component: %u\n",floatComponent);
+	(integerComponent) ? printI(integerComponent) : putchar_cr('0');
 	//printf("Float component:%o\n",floatComponent);
-	printI(integerComponent);
+	//printI(integerComponent);
 	// Print the decimal point
 	putchar_cr('.');
 	unsigned int total = 0;
@@ -156,8 +268,6 @@ void printF(double num)
 		divvy = divvy / 2;
 	}
 	//printf("Total: %u\n",total);
-	char floatSec[10];
-	floatSec[9] = 0;
 	
 	
 	int divisor = 100000000;
@@ -183,6 +293,168 @@ void printF(double num)
 			floatSec[i-1] += 1;
 		}
 		floatSec[i] = 0;
+	}
+	for ( i = 5; i > -1; i-- )
+	{
+		if ( floatSec[i] == ':')
+		{
+			floatSec[i] = '0';
+			floatSec[i-1] += 1; 
+		}
+	}
+	//printf("floatSec: %s\n",floatSec);
+	fputs_cr(floatSec);
+	return;
+}
+
+union convertU
+{
+	double first;
+	long long second;
+} convert;
+
+void printDbl(double num)
+{
+	char floatSec[10];
+	floatSec[9] = 0;
+	double fVal = num;
+	//printf("%f << Original value\n",fVal);
+	convert.first = fVal;
+	unsigned long long intRep = convert.second;
+	//printf("\nINTEGER BITS OF FLOAT:%d\n",intRep);
+	unsigned long long sign = intRep >> 63;
+	//printf("Sign:%u\n",sign);
+	unsigned long long mantissa = ( intRep << 1 ) >> 53;
+	//printf("Mantissa:Octal:%o\nMantissa:Dec:%u\n",mantissa,mantissa);
+	unsigned long long floating = ( ( intRep << 12 ) >> 12 );// | 0b100000000000000000000000;
+	//printf("Float:%u\n",floating);
+	//printf("Float:%o\n",floating);
+	if ( sign != 0 )
+	{
+		putchar_cr('-');
+	}
+	// 0 condition
+	if ( floating == 0 && mantissa == 0 )
+	{
+		floatSec[0] = '0';
+		floatSec[1] = '.';
+		floatSec[2] = '0';
+		floatSec[3] = '0';
+		floatSec[4] = '0';
+		floatSec[5] = '0';
+		floatSec[6] = '0';
+		floatSec[7] = '0';
+		floatSec[8] = 0;
+		fputs_cr(floatSec);
+		return;
+	}
+	// Infinite, and NaN
+	if ( mantissa == 0b11111111 )
+	{
+		// Infinite
+		if ( floating == 0 )
+		{
+			floatSec[0] = 'i';
+			floatSec[1] = 'n';
+			floatSec[2] = 'f';
+			floatSec[3] = 'i';
+			floatSec[4] = 'n';
+			floatSec[5] = 'i';
+			floatSec[6] = 't';
+			floatSec[7] = 'e';
+			floatSec[8] = 0;
+			fputs_cr(floatSec);
+			return;
+		}
+		// NaN, for undefined operations or uninitialized values
+		else
+		{
+			floatSec[0] = 'N';
+			floatSec[1] = 'a';
+			floatSec[2] = 'N';
+			floatSec[3] = 0;
+			fputs_cr(floatSec);
+			return;
+		}
+	}
+	floating |= 0b10000000000000000000000000000000000000000000000000000;
+	//floating |= 0b000000000000000000000000;
+	long long exponent = mantissa - 1023;
+	printf("Floating: %llu\n",floating);
+	printf("Exponent: %lld\n",exponent);
+	unsigned long long integerComponent;
+	unsigned long long floatComponent;
+	if ( exponent < 53 )
+	{
+		integerComponent = (floating >> (52 - exponent));
+		floatComponent = ( floating << ( 12 + exponent ) );
+		printf("<Integer component: %lld\n",integerComponent);
+		printf("<Float component: %llu\n",floatComponent);
+	}
+	else if ( exponent > 52  && exponent < 64)
+	{
+		integerComponent = (floating << (exponent - 52));
+		floatComponent = ( floating << ( 12 + exponent ) );
+		printf("><Integer component: %llu\n",integerComponent);
+		printf("><Float component: %llu\n",floatComponent);
+	}
+	else if ( exponent > 62 )
+	{
+		printf(">Integer component: %lld\n",integerComponent);
+		printf(">Float component: %llu\n",floatComponent);
+	}
+	(integerComponent) ? printL(integerComponent) : putchar_cr('0');
+	//printf("Float component:%o\n",floatComponent);
+	//printI(integerComponent);
+	// Print the decimal point
+	putchar_cr('.');
+	unsigned long long total = 0;
+	long long divvy = 5000000000000000000;
+	unsigned long long mask = 0b1000000000000000000000000000000000000000000000000000000000000000;
+	//printf("\nMask:%o\n",mask);
+	int i;
+	for ( i = 0; i < 52; i++ )
+	{
+		if ( ( ( mask >> i ) & floatComponent ) != 0 )
+		{
+			total += divvy;
+		}
+		divvy = divvy / 2;
+	}
+	//printf("Total: %u\n",total);
+	
+	
+	long long divisor = 1000000000000000000;
+	long long mod;
+	for ( i = 0; divisor >= 1; divisor /= 10, i++ )
+	{
+		mod = total / divisor;
+		total %= divisor;
+		floatSec[i] = mod + '0';
+	}
+	//printf("floatSec: %s\n",floatSec);
+	char promote = 0;
+	// Rounding algorithm
+	for ( i = 18; i > 5; i-- )
+	{
+		promote = 0;
+		if ( floatSec[i] > '4' )
+		{
+			promote = 1;
+		}
+		if ( promote == 1 )
+		{
+			floatSec[i-1] += 1;
+		}
+		floatSec[i] = 0;
+	}
+	for ( i = 5; i > -1; i-- )
+	{
+		if ( floatSec[i] == ':')
+		{
+			floatSec[i] = '0';
+			floatSec[i-1] += 1; 
+		}
 	}
 	//printf("floatSec: %s\n",floatSec);
 	fputs_cr(floatSec);
--- asm/printfasmTest.c
+++ asm/printfasmTest.c
@@ -3,9 +3,22 @@
 //extern printf_cr(char* buf);
 extern printf_cr(char* buf , ...);
 
+union infinite
+{
+	float infin;
+	int infin2;
+} infinite;
+
 int main(int argc , char** argv)
 {
+	int j = 0b01111111100000000000000000000000;
+	infinite.infin2 = j;
 	char* str = "Hello\n%d\n%f\n";
+	//printf("Size of long int: %d\n",sizeof(long long));
+	long long test = 0x7FFFFFFFFFFFFFFF;
+	int test2 = 0x7FFFFFFF;
+	//printf("digits: %lld\n",test);
+	//printf("digits: %d\n",test2);
 	/*
 	printf("int: %d long: %d float: %d double: %d\n",sizeof(int),sizeof(long),sizeof(float),sizeof(double));
 	double x = 54.2345678235;
@@ -16,8 +29,19 @@ int main(int argc , char** argv)
 	int i;
 	for ( i = 0; i < 1; i++ )
 	{
-		printf_cr(str,123456,2.123456789);
-		//printf(str,123456,2.123456789);
+		//printf_cr(str,-123456,infinite.infin);
+		//printf(str,-123456,infinite.infin);
 	}
+	//printf_cr(str,-123456,1234567.1234);
+	//printf(str,-123456,1234567.1234);
+	//printf_cr("-----------------------\n");
+	//printf_cr(str,-123456,.12345678);
+	//printf(str,-123456,.12345678);
+	//printf_cr("-----------------------\n");
+	printf_cr("Dubs: %b\n",12345678901234500000.111);
+	printf("Dubs: %f\n",12345678901234500000.111);
+	printf_cr("-----------------------\n");
+	printf_cr("Dubs: %b\n",123456789.111222333444555666777888999000111222333444555);
+	printf("Dubs: %f\n",123456789.111222333444555666777888999000111222333444555);
 	return 0;
 }
