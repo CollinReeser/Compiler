1bab4f32b9db04449c070fbbb7aa95eb9e8acfae
Collin Reeser <collin@linux-cu6w.site>
2012-02-28 23:08:17 -0500

    So... Actually wrote a useful Pull.sh this time.
    Before, the script just tried to pull into the working directory. Which
    is not where the git directory is. So that fails. Now it cd's into the
    git directory, does the pull, then updates all the working directory
    files with the git directory files.

--- Pull.sh
+++ Pull.sh
@@ -1,1 +1,12 @@
+cd gitCompiler
+
 git pull
+
+cp -u Commit.sh ../
+cp -u Pull.sh ../
+cp -r -u compileSegments ../
+cp -r -u LexParse ../
+cp -r -u tests ../
+cp -r -u utilities ../
+cp -r -u asm ../
+cp -u Makefile ../
