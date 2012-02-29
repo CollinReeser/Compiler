599b2dd0b76f67cf442095c666a530248b11cbfa
Collin Reeser <collin@linux-cu6w.site>
2012-02-28 23:05:28 -0500

    Made an error in the Commit.sh bash file; fixed
    Erroneously named the Pull.sh bash file "Push.sh" in the Commit.sh bash
    file, which is incorrect. Fixed.

--- Commit.sh
+++ Commit.sh
@@ -1,5 +1,5 @@
 cp -u Commit.sh gitCompiler/
-cp -u Push.sh gitCompiler/
+cp -u Pull.sh gitCompiler/
 cp -r -u compileSegments gitCompiler/
 cp -r -u LexParse gitCompiler/
 cp -r -u tests gitCompiler/
