4e61f7b2c0184e7641ef96d9809cdf1443118d4a
Collin <collin.reeser@gmail.com>
2012-03-02 04:04:42 -0500

    Modified development environment to not require foobar setup.
    On a separate development branch, am testing out the actually correct
    use of git where I don't need two copies of the development environment
    with foobar commit and pull scripts. Hopefully this is chill.

--- Commit.sh
+++ Commit.sh
@@ -1,15 +1,3 @@
-cp -u -v Commit.sh gitCompiler/
-cp -u -v .gitignore gitCompiler/
-cp -u -v Pull.sh gitCompiler/
-cp -u -v README gitCompiler/
-cp -r -u -v compileSegments gitCompiler/
-cp -r -u -v LexParse gitCompiler/
-cp -r -u -v tests gitCompiler/
-cp -r -u -v utilities gitCompiler/
-cp -r -u -v asm gitCompiler/
-cp -u -v Makefile gitCompiler/
-
-cd gitCompiler
 ls -A | xargs git add
 git commit -a
 git push -u origin master
--- Pull.sh
+++ Pull.sh
@@ -1,14 +1,1 @@
-cd gitCompiler
-
 git pull
-
-cp -u -v Commit.sh ../
-cp -u -v Pull.sh ../
-cp -u -v .gitignore ../
-cp -u -v README ../
-cp -r -u -v compileSegments ../
-cp -r -u -v LexParse ../
-cp -r -u -v tests ../
-cp -r -u -v utilities ../
-cp -r -u -v asm ../
-cp -u -v Makefile ../
