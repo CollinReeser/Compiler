dab5d6234e2f8ed9d9cee2fb061eafcbd5a37bcd
Collin Reeser <collin.reeser@gmail.com>
2012-03-03 04:29:38 -0500

    Fixed silly echo output mistakes that somehow manifested themselves.
    Even though I definitely fixed them before. Definitely.

--- PanicM.sh
+++ PanicM.sh
@@ -1,4 +1,4 @@
-echo -e "n\NOTE: Panicking on \"master\" branch."
+echo -e "\nNOTE: Panicking on \"master\" branch."
 git checkout master
 git reset ORIG_HEAD --hard
 echo -e "\nNOTE: Current working branch is now \"master\"\n"
